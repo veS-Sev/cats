fe3b42ddf20e36cc660b90a3bcd3b0377b2a0c96
ves-sev <newsecretword@yandex.ru>
2021-05-28 22:41:04 +0300

    html and style added, some js added

---
+++ app/js/main.js
@@ -0,0 +1,71 @@
+$(function () {
+
+  //   var mixer = mixitup(containerEl, {
+
+  //   });
+
+
+  var mixer = mixitup('.select__cards');
+
+  mixer.sort('price-date:asc')
+    .then(function(state) {
+        state.activeSort.attribute === 'price-date';
+    });
+    mixer.sort('age-date:asc')
+    .then(function(state) {
+        state.activeSort.attribute === 'age-date'; // true
+      });
+
+  // кнопка прокрутки вверх 
+let topBtn = document.getElementById('top__btn');
+let getOpacity = (value) => {
+  return topBtn.style.opacity = `${value}`;
+  };
+
+window.addEventListener('scroll', trackScroll);
+topBtn.addEventListener('click', backToTop);
+
+
+function trackScroll() {
+let scroll = window.pageYOffset;
+let coord = document.documentElement.clientHeight;
+       if (scroll > coord) {
+        getOpacity(1);
+    }
+    if (scroll < coord) { 
+      getOpacity(0);
+    
+    } }
+
+   function backToTop() { 
+      if (window.pageYOffset > 0) {
+      window.scrollBy(0, -80);
+      setTimeout(backToTop, 0);
+    }
+  }
+
+
+// Добавляем лайки
+  function addLike() {
+    let buttons = document.getElementsByClassName('select__card-like-svg');
+    for (key of buttons) {
+         key.addEventListener('click', function (fn) {
+        fn.target.closest('.select__card-like-btn').classList.toggle('active');
+      });
+    }
+  };
+  addLike();
+
+
+  // окрашиваем блок в темный цвет, если статус 'Продан'
+  function addColor() {
+    let status = document.querySelectorAll('.select__card-status');
+    status.forEach((elem, i) => {
+      if (elem.innerHTML === 'Продан') {
+        elem.style.backgroundColor = '#1f2021';
+      }
+    })
+  };
+  addColor();
+
+});
